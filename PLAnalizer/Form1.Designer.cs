faa9c8912b47817d54c4703c5fff0656e580c528
Hubert Lewandowski <lewandowski.hubert486@gmail.com>
2018-03-28 19:09:30 +0200

    Basic script reading

--- PLAnalizer/Form1.Designer.cs
+++ PLAnalizer/Form1.Designer.cs
@@ -33,6 +33,7 @@
             this.settingsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.PapyrusOutput = new System.Windows.Forms.RichTextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -44,7 +45,7 @@
             this.aboutToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(420, 24);
+            this.menuStrip1.Size = new System.Drawing.Size(1136, 24);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -69,19 +70,35 @@
             // 
             // PapyrusOutput
             // 
+            this.PapyrusOutput.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.PapyrusOutput.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.PapyrusOutput.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.PapyrusOutput.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.PapyrusOutput.Location = new System.Drawing.Point(0, 24);
             this.PapyrusOutput.Name = "PapyrusOutput";
-            this.PapyrusOutput.Size = new System.Drawing.Size(420, 426);
+            this.PapyrusOutput.ReadOnly = true;
+            this.PapyrusOutput.Size = new System.Drawing.Size(1136, 630);
             this.PapyrusOutput.TabIndex = 1;
             this.PapyrusOutput.Text = "";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Location = new System.Drawing.Point(13, 660);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(1111, 217);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Results of analysis";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(420, 450);
+            this.ClientSize = new System.Drawing.Size(1136, 889);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.PapyrusOutput);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
@@ -102,6 +119,7 @@
         private System.Windows.Forms.ToolStripMenuItem settingsToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
         private System.Windows.Forms.RichTextBox PapyrusOutput;
+        private System.Windows.Forms.GroupBox groupBox1;
     }
 }
 
--- PLAnalizer/Form1.cs
+++ PLAnalizer/Form1.cs
@@ -2,19 +2,103 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 using System.Windows.Forms;
 
 namespace PLAnalizer
 {
     public partial class Form1 : Form
     {
+        /*
+            - Not Important
+            - Papyrus VM Status (3 lines)
+            
+        */
+
+        private new string[] BackColor =
+        {
+            "default" //Error
+        };
+
+        private string[] Color =
+        {
+            "default"
+            //"#E52C1B" //Error
+        };
+
+        private new int[] Font =
+        {
+            2
+        };
+
+
+
+        /// <summary>
+        /// Returns choosen type of font by reading wanted position
+        /// </summary>
+        /// <param name="typeOfFont">0 = regular, 1 = bold, 2 = italic, 3 = underline</param>
+        /// <returns>Choosen type of font</returns>
+        private FontStyle setFont(int typeOfFont)
+        {
+            FontStyle output = new FontStyle();
+            switch(typeOfFont)
+            {
+                case 0: output = FontStyle.Regular; break;
+                case 1: output = FontStyle.Bold; break;
+                case 2: output = FontStyle.Italic; break;
+                case 3: output = FontStyle.Underline; break;
+                default: output = FontStyle.Regular; break;
+            }
+            return output;
+        }
+
         public Form1()
         {
             InitializeComponent();
+            PapyrusOutput.BackColor = SystemColors.Control;
+        }
+
+        private void SyntaxWords()
+        {
+
+            //Pattern array
+            string[] pattern =
+            {
+                "Papyrus\\slog\\sopened\\s.{4,7}|Update budget:.*|Memory page:.*", //Basic info (not important)
+                @"Error",
+            };
+
+            for(int i = 0; i < pattern.Length; i++)
+            {
+                //Setup a collection of words that should be found
+                MatchCollection match = Regex.Matches(PapyrusOutput.Text, pattern[i]);
+
+                //Find all these words
+                foreach (Match word in match)
+                {
+                    //Select matched WORD
+                    PapyrusOutput.Select(word.Index, word.Length);
+
+                    //Edit syntax of this word
+                    try
+                    {
+                        PapyrusOutput.SelectionBackColor =
+                            (BackColor[i] != "default") ? ColorTranslator.FromHtml(BackColor[i]) : PapyrusOutput.BackColor;
+                        PapyrusOutput.SelectionColor =
+                            (Color[i] != "default") ? ColorTranslator.FromHtml(Color[i]) : PapyrusOutput.ForeColor;
+                        PapyrusOutput.SelectionFont = new Font(PapyrusOutput.Font, setFont(Font[i]));
+                    }
+                    catch(IndexOutOfRangeException) {
+                        //MessageBox.Show("Check if ALL elements in config like: Text color, Text background color & Font style are having the same count of items", "Wrong config settings detected!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        return;
+                    }
+                }
+            }
+            
         }
 
         private void OpenFile_Click(object sender, EventArgs e)
@@ -27,7 +111,9 @@ namespace PLAnalizer
             if(FileDialog.ShowDialog() == DialogResult.OK)
             {
                 PapyrusOutput.Text = System.IO.File.ReadAllText(FileDialog.FileName);
+                SyntaxWords();
             }
+
         }
     }
 }
